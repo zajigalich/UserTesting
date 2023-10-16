7ff7d2f146572245a2d30d91064218f76cf9dfb9
Konstantin <titarenko42k@gmail.com>
2023-10-16 16:39:46 +0300

    add exceptions

---
+++ UserTesting/UserTesting.BLL/Exceptions/AppException.cs
@@ -0,0 +1,18 @@
+﻿namespace Gamestore.BLL.Exceptions;
+
+public class AppException : Exception
+{
+    public AppException()
+    {
+    }
+
+    public AppException(string? message)
+        : base(message)
+    {
+    }
+
+    public AppException(string? message, Exception? innerException)
+        : base(message, innerException)
+    {
+    }
+}
---
+++ UserTesting/UserTesting.BLL/Exceptions/UserHasNoAssignedTests.cs
@@ -0,0 +1,11 @@
+﻿using Gamestore.BLL.Exceptions;
+
+namespace UserTesting.BLL.Exceptions;
+
+public class UserHasNoAssignedTests : AppException
+{
+    public UserHasNoAssignedTests(string username)
+        : base($"User {username} has no assigned tests")
+    {
+    }
+}
